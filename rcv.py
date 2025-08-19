1b491fd4d93e413946d6a1fa80002634e97a1fb0
23DarKnight <99487243+23DarKnight@users.noreply.github.com>
2025-08-19 20:31:36 +0530

    Update rcv.py

--- rcv.py
+++ rcv.py
@@ -1,6 +1,12 @@
 import serial
-import time
+import mysql.connector
+import configparser
 
+# Load the configuration file
+config = configparser.ConfigParser()
+config.read("/etc/parth/parth.conf")
+
+id = config["host"]["id"]
 
 ser = serial.Serial(
     port='/dev/ttyUSB0',  # Adjust the port to match your device
@@ -17,8 +23,48 @@ ser.dtr = False  # Deassert DTR for ser2 (receive)
 ser.rts = False  # Deassert RTS for ser2 (receive)
 
 
+def update_teams(data):
+    # Database connection parameters
+    dbname = 'parameters'
+    user = 'gps'
+    password = 'System@68'  # replace with the actual password
+    host = 'localhost'  # replace with the actual host if different
+    port = '3306'  # replace with the actual port if different
+
+    try:
+        # Connect to the database
+        conn = mysql.connector.connect(
+            database=dbname,
+            user=user,
+            password=password,
+            host=host,
+            auth_plugin='mysql_native_password',
+        )
+
+        # Create a cursor object
+        tid, lat, lon = data
+        cur = conn.cursor()
+        cur.execute("""INSERT INTO team (id, latitude, longitude) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE latitude = %s, longitude = %s; """, (tid, round(float(lat),4), round(float(lon), 4), round(float(lat),4), round(float(lon), 4)))
+
+        conn.commit()
+
+        # Close the cursor and connection
+        cur.close()
+        conn.close()
+
+    except mysql.connector.Error as e:
+        print(f"An error occurred: {e}")
+
+
+
 
 while True:
-    msg = ser.readline()
+    msg = ser.readline().decode()
     if msg != "":
         print(msg)
+        if ";" in msg and msg.startswith("b:"):
+            msg = msg.split(";")[0]
+            data = msg.split(":")
+            print(data)
+            update_teams(data)
+            
