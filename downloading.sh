ec2a09ed6a25e34cc42cdbc7c26143d4176d9a6b
23DarKnight <99487243+23DarKnight@users.noreply.github.com>
2025-12-03 14:40:15 +0530

    Update downloading.sh

--- downloading.sh
+++ downloading.sh
@@ -2,9 +2,9 @@ cd /usr/share/tiles/16
 export count=0
 
 export total=1290
-for y in {26947..26976}; do
+for y in {26954..26958}; do
   mkdir $y
-	for x in {46735..46777}; do
+	for x in {45329..47331}; do
 	wget https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/16/$y/$x -O /usr/share/tiles/16/$y/$x &
 	sleep 0.08
 		count=$((count + 1))
